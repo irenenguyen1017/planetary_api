299863b1c3f4d14a6e0bf4384d18a3b50a3d1624
Irene Nguyen <irenenguyen1017@gmail.com>
2022-10-25 15:57:45 +1100

    Added database models

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import os
+from re import S
 
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Float, Integer, String
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__)) # the path for the application itself.
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+basedir = os.path.abspath(
+    os.path.dirname(__file__)
+)  # the path for the application itself.
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "planets.db"
+)
+
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def hello_world():
@@ -42,6 +48,27 @@ def url_variables(name: str, age: int):
         return jsonify(message="Sorry " + name + ", you are not old enough."), 401
     else:
         return jsonify(message="Welcome " + name + ", you are old enough!")
+
+
+# database models
+class User(db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 
 if __name__ == "__main__":
