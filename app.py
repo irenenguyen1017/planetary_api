02fd820c4e1a721e85a3aa9f2e6e3bdc1d968ce3
Irene Nguyen <irenenguyen1017@gmail.com>
2022-10-25 15:13:03 +1100

    Added sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Float, Integer, String
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__)) # the path for the application itself.
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-if __name__ == '__main__':
+
+# Returns a JSON
+@app.route("/super_simple/")
+def super_simple():
+    return jsonify(message="Hello from the Planetary API.")
+
+
+@app.route("/not_found/")
+def not_found():
+    return jsonify(message="That resource is not found."), 404
+
+
+@app.route("/parameters/")
+def parameters():
+    name = request.args.get("name")
+    age = int(request.args.get("age"))
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+
+
+@app.route("/url_variables/<string:name>/<int:age>")
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+
+
+if __name__ == "__main__":
     app.run(debug=True, port=8888)
